cd6d2163d6f00a5db04277ace2bf5c33645e3671
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-06-26 13:24:59 -0600

    create config.js module for PORT

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ console.log('Hello Noteful!');
 // INSERT EXPRESS APP CODE HERE...
 const express = require('express');
 const app = express();
+const { PORT } = require('./config');
 
 // ADD STATIC SERVER HERE
 app.use(express.static('public'));
@@ -30,7 +31,7 @@ app.get('/api/notes/:id', (req, res) => {
 });
 
 
-app.listen(8080, function() {
+app.listen(PORT, function() {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
