4bfc4c25c7276ca7dc7a733d054b1d3a552b1277
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-06-26 12:18:16 -0600

    add api/notes endpoint

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const app = express();
 // ADD STATIC SERVER HERE
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+
 app.listen(8080, function() {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
