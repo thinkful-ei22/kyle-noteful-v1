0d19779d0bbde8e5acddc76d82bc22d47a8a9f30
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-06-27 15:16:23 -0600

    setup router for notes endpoints

--- server.js
+++ server.js
@@ -1,71 +1,27 @@
 'use strict';
 
-// Load array of notes
-const data = require('./db/notes');
-const simDB = require('./db/simDB');
-const notes = simDB.initialize(data);
-
 console.log('Hello Noteful!');
 
-// INSERT EXPRESS APP CODE HERE...
+// IMPORT MODULES
 const express = require('express');
 const app = express();
 const { PORT } = require('./config');
 const morgan = require('morgan');
+const notesRouter = require('./router/notes.router');
 
-// ADD STATIC SERVER HERE
+// LOGGING
 app.use(morgan('dev'));
+
+// STATIC SERVER
 app.use(express.static('public'));
 
 // PARSE REQUEST BODY
 app.use(express.json());
 
-app.get('/api/notes', (req, res, next) => {
-  const { searchTerm } = req.query;
+// ROUTER(S)
+app.use('/api', notesRouter);
 
-  notes.filter(searchTerm, (err, list) => {
-    if (err) {
-      return next(err);
-    }
-    res.json(list);
-  });
-});
-
-app.get('/api/notes/:id', (req, res, next) => {
-  const { id } = req.params;
-  notes.find(id, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    res.json(item);
-  });
-});
-
-app.put('/api/notes/:id', (req, res, next) => {
-  const { id } = req.params;
-
-  /***** Never trust users - validate input *****/
-  const updateObj = {};
-  const updateFields = ['title', 'content'];
-
-  updateFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
-
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
-
+// ERROR HANDLING
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
@@ -80,6 +36,7 @@ app.use(function (err, req, res, next) {
   });
 });
 
+// LISTENER
 app.listen(PORT, function() {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
